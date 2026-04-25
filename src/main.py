84e214dec5538f79ada42b3712f1916522d67e5a
punithkumar686-sketch <punithkumar686@gmail.com>
2026-04-25 13:05:44 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import os
 
-# ensure root output folder exists
 os.makedirs("output", exist_ok=True)
 
 from ai_script import generate_script
@@ -8,15 +7,18 @@ from video import create_video
 from voice import generate_voice
 
 def main():
+
+    # 1. Viral script
     script = generate_script()
-    print("📝 Script:", script)
+    print("SCRIPT:\n", script)
 
+    # 2. Voice
     audio = generate_voice(script)
-    print("🔊 Audio created:", audio)
 
+    # 3. Video
     video_path = create_video(script, audio)
 
-    print("🎬 Video created at:", video_path)
+    print("VIDEO READY:", video_path)
 
 if __name__ == "__main__":
     main()
