8c14391fdced8de5dcbd8bf115388fb187bd010a
punithkumar686-sketch <punithkumar686@gmail.com>
2026-04-25 16:36:00 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,13 @@
 from ai_script import generate_script
 from video import create_video
-from voice import generate_voice
 
 def main():
 
     script = generate_script()
     print("SCRIPT:\n", script)
 
-    voice = generate_voice(script)
-
-    create_video(script, voice, "mobile")
-    create_video(script, voice, "desktop")
+    create_video(script, None, "mobile")
+    create_video(script, None, "desktop")
 
 if __name__ == "__main__":
     main()
